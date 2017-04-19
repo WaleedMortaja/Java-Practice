6541919e64d9716c614cb49aac9dce736a28c234
Waleed Mortaja <waleed.mortaja@gmail.com>
2017-04-19 21:21:46 +0300

    Create E11.java

---
+++ Chapter3/E11.java
@@ -0,0 +1,58 @@
+/*
+E3.11 Implement a class Employee. An employee has a name (a string) and a salary (a double).
+Provide a constructor with two arguments
+public Employee(String employeeName, double currentSalary)
+and methods
+public String getName()
+public double getSalary()
+public void raiseSalary(double byPercent)
+These methods return the name and salary, and raise the employee’s salary by a
+certain percentage. Sample
+usage:
+Employee harry = new Employee("Hacker, Harry", 50000);
+harry.raiseSalary(10); // Harry gets a 10 percent raise
+Supply an EmployeeTester class that tests all methods.
+*/
+
+
+
+
+/**
+ * Calculate employees’ salaries. 
+ */
+public class Employee
+{
+    private final String name;
+    private double salary;
+    
+    /**
+    * Constructor for an employee.
+    * @param employeeName employee name
+    * @param currentSalary employee current salary
+    */
+    public Employee(String employeeName, double currentSalary)
+    {
+    name = employeeName;
+    salary=currentSalary;
+    }
+    /**
+     * return employee name
+      *@return name the name of the employee
+      */
+    public String getName(){return name;}
+    /**
+     * return employee salary
+      *@return salary the salary of the employee
+      */
+    
+    public double getSalary(){return salary;}
+    /**
+     *Increase salary by the given value.
+     *@param byPercent the percentage which with the employee salary will increase
+     */
+    
+    public void raiseSalary(double byPercent)
+    {
+        salary += salary*byPercent/100.0;
+    }
+}
